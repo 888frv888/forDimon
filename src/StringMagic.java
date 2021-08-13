9ae0e813c1bfbe1ba924cab99c91cacac4812a39
888frv888 <888frv888@gmail.com>
2021-08-14 01:28:27 +0300

    Optimizing code

--- src/StringMagic.java
+++ src/StringMagic.java
@@ -1,13 +1,18 @@
 public class StringMagic {
     public static void main(String[] args) {
         String startString = "kj12n44 prom669j0f kjn55lk893kj234";
-        char[] charArray;
-        charArray = new char[startString.length()];
+        char[] charArray = new char[startString.length()];
         for (byte i =0; i < charArray.length; i++) {
             charArray[i] = startString.charAt(i);
-            if (Character.isLetter(charArray[i])) System.out.println("символ " + charArray[i] + " по индексу " + i + " является буквой");
-            else if (Character.isDigit(charArray[i])) System.out.println("символ " + charArray[i] +  " по индексу " + i + " является цифрой");
-            else System.out.println("символ "+charArray[i]+ " по индексу " + i + " скорее всего пробел");
+            if (Character.isLetter(charArray[i])) {
+                System.out.println("символ " + charArray[i] + " по индексу " + i + " является буквой");
+            }
+            else if (Character.isDigit(charArray[i])) {
+                System.out.println("символ " + charArray[i] +  " по индексу " + i + " является цифрой");
+            }
+            else {
+                System.out.println("символ "+charArray[i]+ " по индексу " + i + " скорее всего пробел");
+            }
         }
     }
 }
